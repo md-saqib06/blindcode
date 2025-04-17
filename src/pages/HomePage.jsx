9d40eafc9fae81ec548e04fe6072660d395b12e3
Rohan Kumar Singh <85818124+rohankrsingh@users.noreply.github.com>
2025-04-17 13:45:11 +0530

    particles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,10 +5,22 @@ import HeroSection from '@/components/herosection/HeroSection'
 
 const HomePage = () => {
   return (
-    <div>
-      <Navbar/>
-      <HeroSection/>
+    <div className='bg-zinc-950'>
+      <div className='relative overflow-hidden w-screen h-screen bg-violet-950/20'>
+      <Navbar />
+      <HeroSection />
+      <Particles
+        className="absolute inset-0 z-0 w-full h-screen"
+        quantity={120}
+        ease={80}
+        color="#fff"
+        vx={0.1}
+        vy={0.8}
+        refresh={true} />
+        
     </div>
+    </div>
+    
   )
 }
 
