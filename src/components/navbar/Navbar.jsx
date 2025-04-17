e3b42f8518080e97ee1a3427d88c21aaed23986b
md-saqib06 <mohdsaqib.rx@gmail.com>
2025-04-17 13:19:09 +0530

    Minor Changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full h-20 bg-purple-700/80'>
+      <ul>
+        <li><img src="@/assets/NVLogo.webp" alt="" /></li>
+        <li><img src="@/assets/IITMLogo.webp" alt="" /></li>
+        <li> Celestia 1.0</li>
+      </ul>
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </nav>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '@/components/navbar/Navbar'
+import HeroSection from '@/components/herosection/HeroSection'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Navbar/>
+      <HeroSection/>
+    </div>
   )
 }
 
