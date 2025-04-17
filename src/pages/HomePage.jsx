774f5d25bb87c056910640b794be88d248b18ed9
Rohan Kumar Singh <85818124+rohankrsingh@users.noreply.github.com>
2025-04-17 13:16:43 +0530

    animate import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
+import { Particles } from '@/components/magicui/particles'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+      <Particles 
+      className="absolute inset-0 z-0"
+      quantity={100}
+      ease={80}
+      color="#ffffff"
+      refresh/>
+    </div>
   )
 }
 
