5042cb47f36e8e049271ebd7bda96ec2831eabc0
Rohan Kumar Singh <85818124+rohankrsingh@users.noreply.github.com>
2025-04-17 14:01:22 +0530

    herosection

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className='min-h-[90vh] w-screen flex flex-col items-center justify-center space-y-6 absolute top-0 inset-0'>
+      <h1 className='text-6xl text-violet-950/90 text-shadow-cyan-300 text-shadow-2xs backdrop-b'>
+      BlindCode
+      </h1>
+      <h4 className='text-xl font-medium max-w-[800px] text-white'>
+        In this Event, you will be give a demo landing page that youi will'll have to create
+      </h4>
+    </div>
   )
 }
 
